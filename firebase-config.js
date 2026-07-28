1466e3403a35b2fd1baf518cc4a5f8c866ba58cb
srpkagitha-max <srpkagitha@gmail.com>
2026-07-28 18:55:01 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
-// KSR EXAM OS+ Firebase Config
+// KSR EXAM OS+ V5 - Firebase Configuration
 window.KSR_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyABdatIBfO0AEr_POVUH40_UMrixyRisdMA",
+  apiKey: "AIzaSyBdatBifOOAEr_POVUH40_UMrixyRisdMA",
   authDomain: "ksr-exam-os-v5.firebaseapp.com",
   projectId: "ksr-exam-os-v5",
   storageBucket: "ksr-exam-os-v5.firebasestorage.app",
